0a9804dd151db8bb5f128a0a45cb14cd86ad18a5
Ruben <ruben@pointbreak.co>
2020-10-31 17:42:51 +0100

    changed dist css file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { App } from './components/App'
 import PropTypes from 'prop-types'
-import styles from './styles.module.css'
 
 export const AppOne = ({ text, searchInput }) => {
   return <App text={text} searchInput={searchInput} />
