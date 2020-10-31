f51947752971539263715dd5febd4069731fabba
Ruben <ruben@pointbreak.co>
2020-10-31 15:18:31 +0100

    Updates

---
+++ precache-manifest.c28640e14d0eefa6f5d9947868b26840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af2032b5be02afeb2dd0491abd17f26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30e96ffbe87804c03f90",
+    "url": "./static/css/main.7c5b9810.chunk.css"
+  },
+  {
+    "revision": "415184695d26df6d3cc8",
+    "url": "./static/js/2.18ddfa2b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18ddfa2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e96ffbe87804c03f90",
+    "url": "./static/js/main.9840e6f7.chunk.js"
+  },
+  {
+    "revision": "159bc56009a1d87ae6f3",
+    "url": "./static/js/runtime-main.cb1e2e75.js"
+  }
+]);
