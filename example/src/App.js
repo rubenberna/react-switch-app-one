683f837ef190c71ab8cfadd6fd600af70e7a4c39
Ruben <ruben@pointbreak.co>
2020-10-31 19:46:16 +0100

    reverted to components

--- example/src/App.js
+++ example/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { GithubProfileSearch } from '@rubenberna/react-switch-app-one'
+import { AppOne } from '@rubenberna/react-switch-app-one'
 import '@rubenberna/react-switch-app-one/dist/index.css'
 
 const App = () => {
-  return <GithubProfileSearch text="Please type a GitHub username"/>
+  return <AppOne text="Please type a GitHub username"/>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,82 +1,12 @@
-// import React from 'react'
-// import { App } from './components/App'
-// import PropTypes from 'prop-types'
-// import styles from './styles.module.scss'
-//
-// export const AppOne = ({ text, searchInput }) => {
-//   return <App text={text} searchInput={searchInput} />
-// }
-//
-// App.propTypes = {
-//   searchInput: PropTypes.string,
-//   text: PropTypes.string
-// }
-
-import React, { useState } from 'react'
+import React from 'react'
+import { App } from './components/App'
 import PropTypes from 'prop-types'
-import styles from './styles.module.scss'
-import axios from 'axios'
-import BeatLoader from 'react-spinners/ClipLoader'
 
-const Header = ({ text }) => (
-  <div className={styles.textCenter}>
-    <h2 className={styles.header}>App One</h2>
-    <p>{text}</p>
-  </div>
-)
-
-export const GithubProfileSearch = ({ searchInput, text }) => {
-  const [profile, setProfile] = useState()
-  const [loading, setLoading] = useState(false)
-
-  const fetchProfile = async (e) => {
-    e.preventDefault()
-    setLoading(true)
-    const res = await axios.get(`https://api.github.com/users/${searchInput}`)
-    setLoading(false)
-    setProfile(res.data)
-  }
-
-  const renderProfile = () => (
-    <div className={styles.githubProfile}>
-      <img
-        className={styles.githubProfileAvatar}
-        src={profile.avatar_url}
-        alt='github avatar'
-      />
-      <div>
-        <p>
-          <span style={{ fontWeight: 600 }}>Name: </span>
-          <span>{profile.name}</span>
-        </p>
-        <p>
-          <span style={{ fontWeight: 600 }}>Login: </span>
-          <span>{profile.name}</span>
-        </p>
-      </div>
-    </div>
-  )
-
-  return (
-    <React.Fragment>
-      <Header text={text} />
-      <div className={styles.flexColumnCentered}>
-        {searchInput && (
-          <p>
-            Search input: <span className={styles.italic}>{searchInput}</span>
-          </p>
-        )}
-        <button onClick={fetchProfile} type={'submit'}>
-          Search Github profiles
-        </button>
-        <BeatLoader size={70} color='#399D8B' loading={loading} />
-        {profile && renderProfile()}
-      </div>
-    </React.Fragment>
-  )
+export const AppOne = ({ text, searchInput }) => {
+  return <App text={text} searchInput={searchInput} />
 }
 
-GithubProfileSearch.propTypes = {
+App.propTypes = {
   searchInput: PropTypes.string,
   text: PropTypes.string
 }
